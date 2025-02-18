ff08f6188c9764f5f6c73292feb3a81e0c510d8a
njg4ne <njg4ne@virginia.edu>
2025-02-18 16:21:33 -0500

    fixing html volume together on zoom

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,7 @@ mkdir -p volumes/final-project/config
 rm -rf volumes/final-project/config/*
 mkdir -p volumes/home-page/{config,html}
 rm -rf volumes/home-page/{config,html}/*
+rm -rf volumes/home-page/html
 
 docker pull nginx:alpine3.21
 docker run --rm --name temp-nginx -d nginx:alpine3.21
@@ -13,3 +14,7 @@ docker cp temp-nginx:/etc/nginx/nginx.conf volumes/final-project/config/nginx.co
 
 docker cp temp-nginx:/etc/nginx/conf.d volumes/home-page/config
 docker cp temp-nginx:/etc/nginx/nginx.conf volumes/home-page/config/nginx.conf
+
+docker cp temp-nginx:/usr/share/nginx/html volumes/home-page/html
+
+docker stop temp-nginx
